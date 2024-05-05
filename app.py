ab84ff9dd838721b9055b92e6c6d048a6ea4c008
JapiKredi <jasper.bongers@yahoo.com>
2024-05-05 15:20:38 +0800

    Update code

--- app.py
+++ app.py
@@ -34,3 +34,39 @@ settings = {
     "presence_penalty": 0,
     "stop": ["```"],
 }
+
+@cl.step(type="llm", root=True, language="sql")
+async def text_to_sql(text: str):
+    # Create a ChatGeneration to enable the prompt playground
+    generation = cl.ChatGeneration(
+        provider=ChatOpenAI.id,
+        messages=[
+            {
+                "role": "user",
+                "content": template.format(input=text),
+            }
+        ],
+        settings=settings,
+        variables={"input": text},
+    )
+
+    # Call OpenAI
+    stream = await client.chat.completions.create(
+        messages=generation.messages, stream=True, **settings
+    )
+
+    current_step = cl.context.current_step
+
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await current_step.stream_token(token)
+
+    generation.completion = current_step.output
+
+    current_step.generation = generation
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    await text_to_sql(message.content)
+
